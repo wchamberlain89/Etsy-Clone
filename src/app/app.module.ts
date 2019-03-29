bd5a07591507b075e0c5ca2c45909dacdf8978ec
Student <example@example.com>
2019-03-29 08:43:58 -0700

    Header Bar component HTML setup, Also will contain base files for icon-link and search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { IconLinkComponent } from './icon-link/icon-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    IconLinkComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/icon-link/icon-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-link',
+  templateUrl: './icon-link.component.html',
+  styleUrls: ['./icon-link.component.scss']
+})
+export class IconLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
