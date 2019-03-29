80650db042267b94e8ca0d7e8ed75b8c19f4c95a
Student <example@example.com>
2019-03-29 11:28:18 -0700

    it has discover and cart displayed in header component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  public color:string = 'default';
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
