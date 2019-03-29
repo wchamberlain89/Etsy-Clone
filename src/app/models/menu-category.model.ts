e2c8fb4d5f14cf25b4ad2e07ff1fa3ad156656ca
Student <example@example.com>
2019-03-29 13:15:21 -0700

    It will have MenuCategory class setup

---
+++ src/app/models/menu-category.model.ts
@@ -0,0 +1,9 @@
+export Class MenuCategory {
+  constructor(
+    public name:string;
+    public subCategories:[]subCategory;
+    public href:string;
+    public img:string;
+    public editorsPick:string;
+  ){}
+}
